a18f5bb34561935972ecf454c6e3f75bb9ea44e8
glonghini <43152762+glonghini@users.noreply.github.com>
2019-04-13 20:14:24 -0300

    Initial commit

---
+++ api.py
@@ -0,0 +1,12 @@
+from flask import flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def hello():
+    return ("oi dona leticia")
+
+if __name__ == "__main__":
+    app.run(debug=True)
